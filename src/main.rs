feb80637cb0ce92f5c9134d42eee920b21b5b76a
Paulo Elienay <elie9alb@gmail.com>
2021-05-18 15:01:52 -0300

    added `cd`

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,11 @@
+use std::env;
 use std::io::{self, Write};
+use std::path::Path;
 use std::process::Command;
 
 fn main() {
     loop {
-        print!("%");
+        print!("% ");
         io::stdout().flush().unwrap();
 
         let mut input = String::new();
@@ -12,8 +14,18 @@ fn main() {
         let mut parts = input.trim().split_whitespace();
         let command = parts.next().unwrap();
 
-        let mut child = Command::new(command).args(parts).spawn().unwrap();
-
-        child.wait();
+        match command {
+            "cd" => {
+                let new_dir = parts.peekable().peek().map_or("/", |x| *x);
+                let root = Path::new(new_dir);
+                if let Err(e) = env::set_current_dir(&root) {
+                    eprintln!("{}", e);
+                }
+            }
+            command => {
+                let mut child = Command::new(command).args(parts).spawn().unwrap();
+                child.wait();
+            }
+        }
     }
 }
